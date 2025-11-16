1fc6c3d6839518ae76233865ece2741a39864d90
Luis Miguel Hadzich <lm.hadzich@gmail.com>
2025-11-16 15:04:53 -0500

    Fix database configuration for production

--- config/database.js
+++ config/database.js
@@ -1,13 +1,37 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  // For production (Render)
+  if (env('NODE_ENV') === 'production') {
+    const config = parse(env('DATABASE_URL') || '');
+    return {
+      connection: {
+        client: 'postgresql',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        pool: {
+          min: 0,
+          max: 1
+        },
+        debug: false,
+      },
+    };
+  }
+
+  // For development (SQLite)
+  return {
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: 'sqlite',
+      connection: {
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      useNullAsDefault: true,
     },
-  },
-});
+  };
+};
